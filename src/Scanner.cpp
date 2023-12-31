56f131ad45deb34096532605104e798889aa3c63
gskapoor <kapoorgobind@gmail.com>
2023-12-30 23:16:18 -0800

    some basic scanning :)

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -3,7 +3,19 @@
 Scanner::Scanner(std::string p_source)
   :source(p_source){}
 
-std::vector<Token> scanTokens() {
+std::vector<Token> Scanner::scanTokens() {
 
+  while (!isAtEnd()) {
+    start = current;
+    // scanToken();
+  }
 
+  tokens.emplace_back(new Token(
+    TokenType::LOX_EOF, "", std::nullopt, line ));
+
+  return tokens;
+}
+
+bool Scanner::isAtEnd() {
+  return current >= source.size();
 }
