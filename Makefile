43b00dd09d796f6821b6594b6f091b5354dea8bf
gskapoor <kapoorgobind@gmail.com>
2023-12-30 16:57:19 -0800

    Basic REPL stuff

---
+++ Makefile
@@ -0,0 +1,23 @@
+CXX := g++
+CXXFLAGS := -std=c++23
+SRCDIR := src
+BUILDDIR := build 
+TARGET := main.out
+
+SRCEXT := cpp
+SOURCES := $(wildcard $(SRCDIR)/*.$(SRCEXT)) # GET ALL SOURCE FILES
+OBJECTS := $(patsubst $(SRCDIR)/%,$(BUILDDIR)%,$(SOURCES:.$(SRCEXT)=.o))
+
+all: $(TARGET)
+
+main.out: main.o
+	$(CXX) -o main.out main.o
+
+main.o: main.cpp
+	$(CXX) -c main.cpp
+
+clean:
+	@echo " Cleaning..."; 
+	rm -f *.o *~ main
+
+.PHONY: clean
--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+void runFile(std::string path) {
+
+  std::ifstream inputFile(path);
+
+  if (!inputFile.is_open()){
+    std::cerr << "Error opening file: " << path << std::endl;
+    return;
+  }
+
+  std::string fileContent(
+    (std::istreambuf_iterator<char>(inputFile)),
+    (std::istreambuf_iterator<char>())
+  );
+
+  inputFile.close();
+
+  std::cout << "File Content\n" << fileContent << std::endl;
+
+  return;
+}
+
+void runPrompt() {
+  std::cout << ">> ";
+
+  std::string line;
+
+  while (std::getline(std::cin, line) && !line.empty()) {
+    std::cout << line << std::endl;
+    
+    std::cout << ">> ";
+  }
+}
+
+int main(int argc, char* argv[]) {
+
+  if (argc > 2){
+    std::cerr << "Usage: " << argv[0] << " <script> " << std::endl;
+    return 64;
+  } else if (argc == 2){
+    runFile(argv[1]);
+  } else {
+    runPrompt();
+  }
 
   return 0;
 }
